11b82a0cad30b28f3a2ee0de78544fe32adc4b1c
Steve Li <lithafnium2000@gmail.com>
2017-10-23 19:51:25 -0400

    node js stuff

---
+++ js/server.js
@@ -0,0 +1,19 @@
+const https = require("https"); 
+
+const url = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBDNGE0_gOFq0YbFjSB9OxtdwBcHSRm5_s"
+
+https.get(url, res => {
+  res.setEncoding("utf8");
+  let body = "";
+  res.on("data", data => {
+    body += data;
+  });
+  res.on("end", () => {
+    body = JSON.parse(body);
+    console.log(
+      `City: ${body.results[0].formatted_address} -`,
+      `Latitude: ${body.results[0].geometry.location.lat} -`,
+      `Longitude: ${body.results[0].geometry.location.lng}`
+    );
+  });
+});
