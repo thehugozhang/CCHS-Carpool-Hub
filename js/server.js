ee9c7dbf6c16085bb82092075417f257552de95b
Steve Li <lithafnium2000@gmail.com>
2017-10-23 19:58:57 -0400

    added firebase and geocoding functionality to node js stuff

--- js/server.js
+++ js/server.js
@@ -1,6 +1,25 @@
 const https = require("https"); 
+var firebase = require('firebase'); 
+
+var config = {
+  apiKey: "AIzaSyADnVGr84Qq2qeRz5vc2bZ3MQbjJ6GKSOU",
+  authDomain: "cchs-carpool.firebaseapp.com",
+  databaseURL: "https://cchs-carpool.firebaseio.com",
+  projectId: "cchs-carpool",
+  storageBucket: "cchs-carpool.appspot.com",
+  messagingSenderId: "136028210350"
+};
+firebase.initializeApp(config);
 
 const url = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBDNGE0_gOFq0YbFjSB9OxtdwBcHSRm5_s"
+
+var name = "Cali";
+firebase.database().ref('Students/' + name).set({
+    address: "1600 Amphitheatre Parkway",
+    town: "Mountain View", 
+    state: "CA", 
+    zipcode: "94043"
+});
 
 https.get(url, res => {
   res.setEncoding("utf8");
